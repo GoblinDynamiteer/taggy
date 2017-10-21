8b62c869f0b39c27aa52a77d9a3df4ebc3e1a116
Johan Kämpe <kampe.johan@gmail.com>
2017-10-21 17:13:27 +0200

    Init

---
+++ taggy/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace taggy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textboxTagInfo.Clear();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "FLAC Files (*.flac)|*.flac|MP3 Files (*.mp3)|*.mp3|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var file = TagLib.File.Create(openFileDialog.FileName);
+                    var year = file.Tag.Year;
+                    var track = file.Tag.Track;
+                    var title = file.Tag.Title;
+                    var artist = file.Tag.FirstArtist;
+                    var album = file.Tag.Album;
+
+                    textboxTagInfo.Clear();
+                    textboxTagInfo.AppendText(
+                        year.ToString() + "\r\n" +
+                        title.ToString() + "\r\n" +
+                        track.ToString() + "\r\n" +
+                        album.ToString() + "\r\n" +
+                        artist.ToString() + "\r\n"
+                        );
+
+                    file.Save();
+                }
+
+                catch
+                {
+                }
+            }
+
+        }
+    }
+}
